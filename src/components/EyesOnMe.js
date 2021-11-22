7421bade723081bed80fdb72c1c1be136aa92bad
Drew Keat <thedrewkeat@gmail.com>
2021-11-21 21:42:34 -0700

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React, { Component } from "react";
+
+export default class EyesOnMe extends Component {
+
+    hasFocus = () => {
+        console.log('Good!')
+    }
+
+    notFocus = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+  render() {
+    return (
+        <button onFocus={this.hasFocus} onBlur={this.notFocus}>Pay Attention</button>
+    )
+  }
+}
